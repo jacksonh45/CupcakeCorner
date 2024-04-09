1bc63c6df87366a15e81545130a2db258b7ded5f
Jackson Harrison <Jackson.Harrison@stu.barren.kyschools.us>
2024-04-09 08:06:01 -0500

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Jackson Harrison on 4/9/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
