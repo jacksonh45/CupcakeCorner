d0ac835649ac9d462c11057998c57772d8d324d5
Jackson Harrison <Jackson.Harrison@stu.barren.kyschools.us>
2024-04-25 08:12:28 -0500

    Project completed

---
+++ CupcakeCorner/AddressView.swift
@@ -0,0 +1,18 @@
+//
+//  AddressView.swift
+//  CupcakeCorner
+//
+//  Created by Jackson Harrison on 4/25/24.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddressView()
+}
---
+++ CupcakeCorner/Order.swift
@@ -0,0 +1,18 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Jackson Harrison on 4/25/24.
+//
+
+import SwiftUI
+
+struct Order: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Order()
+}
